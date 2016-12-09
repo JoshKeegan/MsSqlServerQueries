d9afa05a72c8a2de037b52041789dfa143c1b92f
Josh Keegan <josh@visav.co.uk>
2016-12-09 16:28:09 +0000

    new Data Stored per Index query

---
+++ Diagnostics/Data Stored per Index.sql
@@ -0,0 +1,27 @@
+/*
+	Data Stored per Index
+	Authors:
+		Josh Keegan 09/12/2016
+
+	Requires PrettyPrintDataSize
+*/
+
+DECLARE @tableName nvarchar(max) = 'TwitterStatuses';
+DECLARE @pageBytes AS int = 8 * 1024;
+
+SELECT 
+	i.name AS Name,
+	i.type_desc AS 'Type',
+	i.is_unique AS 'Unique',
+	i.is_primary_key AS 'PK',
+	[master].[dbo].[PrettyPrintDataSize](au.total_pages * @pageBytes) AS 'Total Space', 
+	[master].[dbo].[PrettyPrintDataSize](au.used_pages * @pageBytes) AS 'Used Space', 
+	[master].[dbo].[PrettyPrintDataSize]((au.total_pages - au.used_pages) * @pageBytes) AS 'Unused Space'
+FROM sys.tables t
+INNER JOIN sys.indexes i ON i.object_id = t.object_id
+INNER JOIN sys.partitions p ON p.object_id = t.object_id AND p.index_id = i.index_id
+INNER JOIN sys.allocation_units au ON au.container_id = p.partition_id
+WHERE t.is_ms_shipped = 0
+AND t.name = @tableName
+AND au.type = 1 /* In row data, so excludes things like LOB data */
+ORDER BY au.total_pages DESC
