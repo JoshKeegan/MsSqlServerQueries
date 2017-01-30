b207f75e519499492408c6a7010b261ae9ec5fb2
Josh Keegan <josh@visav.co.uk>
2017-01-30 11:45:08 +0000

    Include partition compression level in Data Stored per Table query

--- Diagnostics/Data Stored per Table.sql
+++ Diagnostics/Data Stored per Table.sql
@@ -8,14 +8,15 @@
 DECLARE @pageBytes AS int = 8 * 1024;
 
 SELECT 
-s.Name AS 'Schema',
-t.NAME AS 'Table',
-p.rows AS 'Num Rows',
-[master].[dbo].[PrettyPrintDataSize](SUM(a.total_pages) * @pageBytes) AS 'Total Space', 
-[master].[dbo].[PrettyPrintDataSize](SUM(a.used_pages) * @pageBytes) AS 'Used Space', 
-[master].[dbo].[PrettyPrintDataSize]((SUM(a.total_pages) - SUM(a.used_pages)) * @pageBytes) AS 'Unused Space',
-[master].[dbo].[PrettyPrintDataSize](SUM(CASE WHEN i.index_id IN (0, 1) THEN a.total_pages ELSE NULL END) * @pageBytes) AS 'Total Data Size',
-[master].[dbo].[PrettyPrintDataSize](COALESCE(SUM(CASE WHEN i.index_id NOT IN (0, 1) THEN a.total_pages ELSE NULL END) * @pageBytes, 0)) AS 'Total NC Indexes Size'
+	s.Name AS 'Schema',
+	t.NAME AS 'Table',
+	p.rows AS 'Num Rows',
+	[master].[dbo].[PrettyPrintDataSize](SUM(a.total_pages) * @pageBytes) AS 'Total Space', 
+	[master].[dbo].[PrettyPrintDataSize](SUM(a.used_pages) * @pageBytes) AS 'Used Space', 
+	[master].[dbo].[PrettyPrintDataSize]((SUM(a.total_pages) - SUM(a.used_pages)) * @pageBytes) AS 'Unused Space',
+	[master].[dbo].[PrettyPrintDataSize](SUM(CASE WHEN i.index_id IN (0, 1) THEN a.total_pages ELSE NULL END) * @pageBytes) AS 'Total Data Size',
+	[master].[dbo].[PrettyPrintDataSize](COALESCE(SUM(CASE WHEN i.index_id NOT IN (0, 1) THEN a.total_pages ELSE NULL END) * @pageBytes, 0)) AS 'Total NC Indexes Size',
+	p.data_compression_desc AS 'Compression'
 FROM sys.tables t
 INNER JOIN sys.schemas s ON s.schema_id = t.schema_id
 INNER JOIN sys.indexes i ON t.OBJECT_ID = i.object_id
@@ -24,6 +25,6 @@ INNER JOIN sys.allocation_units a ON p.partition_id = a.container_id
 WHERE t.NAME NOT LIKE 'dt%' -- filter out system tables for diagramming
 AND t.is_ms_shipped = 0
 AND i.OBJECT_ID > 255 
-GROUP BY t.Name, s.Name, p.Rows
+GROUP BY t.Name, s.Name, p.Rows, p.data_compression_desc
 --ORDER BY s.Name, t.Name
 ORDER BY SUM(a.total_pages) DESC
