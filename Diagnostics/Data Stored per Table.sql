feba5aca2eec45c3515517c538fc3b85a7e8c03c
Josh Keegan <josh@visav.co.uk>
2017-04-11 11:11:18 +0100

    Correct compression field for Data Stored Per Table query. Since indexes in a table could have different compression levels it was causing some tables to ave multiple rows

--- Diagnostics/Data Stored per Table.sql
+++ Diagnostics/Data Stored per Table.sql
@@ -7,24 +7,54 @@
 
 DECLARE @pageBytes AS int = 8 * 1024;
 
+WITH CompressionsInUse AS
+(
+	SELECT DISTINCT
+		i.object_id,
+		data_compression_desc
+	FROM sys.indexes i
+	INNER JOIN sys.partitions p ON i.object_id = p.object_id AND i.index_id = p.index_id
+),
+CompressionPerTable AS
+(
+	SELECT 
+		t.object_id,
+		(
+			SELECT CASE WHEN 
+			(
+				SELECT COUNT(*) 
+				FROM CompressionsInUse 
+				WHERE CompressionsInUse.object_id = t.object_id
+			) > 1 
+			THEN 'MIXED' 
+			ELSE 
+			(
+				SELECT TOP 1 data_compression_desc 
+				FROM CompressionsInUse 
+				WHERE CompressionsInUse.object_id = t.object_id
+			) END
+		) AS compressionsInUse
+	FROM sys.tables t
+)
 SELECT 
-	s.Name AS 'Schema',
-	t.NAME AS 'Table',
+	s.name AS 'Schema',
+	t.name AS 'Table',
 	p.rows AS 'Num Rows',
 	[master].[dbo].[PrettyPrintDataSize](SUM(a.total_pages) * @pageBytes) AS 'Total Space', 
 	[master].[dbo].[PrettyPrintDataSize](SUM(a.used_pages) * @pageBytes) AS 'Used Space', 
 	[master].[dbo].[PrettyPrintDataSize]((SUM(a.total_pages) - SUM(a.used_pages)) * @pageBytes) AS 'Unused Space',
-	[master].[dbo].[PrettyPrintDataSize](SUM(CASE WHEN i.index_id IN (0, 1) THEN a.total_pages ELSE NULL END) * @pageBytes) AS 'Total Data Size',
-	[master].[dbo].[PrettyPrintDataSize](COALESCE(SUM(CASE WHEN i.index_id NOT IN (0, 1) THEN a.total_pages ELSE NULL END) * @pageBytes, 0)) AS 'Total NC Indexes Size',
-	p.data_compression_desc AS 'Compression'
+	[master].[dbo].[PrettyPrintDataSize](SUM(CASE WHEN i.type IN (0, 1) THEN a.total_pages ELSE NULL END) * @pageBytes) AS 'Total Data Size',
+	[master].[dbo].[PrettyPrintDataSize](COALESCE(SUM(CASE WHEN i.type NOT IN (0, 1) THEN a.total_pages ELSE NULL END) * @pageBytes, 0)) AS 'Total NC Indexes Size',
+	CompressionPerTable.compressionsInUse AS 'Compression'
 FROM sys.tables t
 INNER JOIN sys.schemas s ON s.schema_id = t.schema_id
-INNER JOIN sys.indexes i ON t.OBJECT_ID = i.object_id
-INNER JOIN sys.partitions p ON i.object_id = p.OBJECT_ID AND i.index_id = p.index_id
+INNER JOIN sys.indexes i ON t.object_id = i.object_id
+INNER JOIN sys.partitions p ON i.object_id = p.object_id AND i.index_id = p.index_id
 INNER JOIN sys.allocation_units a ON p.partition_id = a.container_id
-WHERE t.NAME NOT LIKE 'dt%' -- filter out system tables for diagramming
+INNER JOIN CompressionPerTable ON CompressionPerTable.object_id = t.object_id
+WHERE t.name NOT LIKE 'dt%' -- filter out system tables for diagramming
 AND t.is_ms_shipped = 0
-AND i.OBJECT_ID > 255 
-GROUP BY t.Name, s.Name, p.Rows, p.data_compression_desc
+AND i.object_id > 255
+GROUP BY t.name, s.name, p.rows, CompressionPerTable.compressionsInUse
 --ORDER BY s.Name, t.Name
 ORDER BY SUM(a.total_pages) DESC
